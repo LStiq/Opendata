8be50f815346698d61738069b9601c0c04639a82
LStiq <louis.stiquel@sfr.fr>
2024-05-03 09:10:42 +0200

    màj

--- Projet.py
+++ Projet.py
@@ -3,10 +3,10 @@ import tkinter, tkintermapview, math, requests
 
 def get_geojson(url):
     response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
+    if response.status_code == 200: #On récupère la réponse get
+        return response.json() #On retourne un json
     else:
-        print(f"Failed to load data from {url}")
+        print(f"Erreur pour charger les données de {url}") #Si on arrive pas à récupérer alors msg d'erreur
         return None
     
 def haversine_distance(lat1, lon1, lat2, lon2):
@@ -53,8 +53,9 @@ lignes_dict = {}
 arrets_dict = {}
 troncon_dict = {}
 horaires_arrets_dict = {}
-
 relation_troncon_chemin_dict = {}
+
+# Listes pour stocker des informations nécessaire à l'affichage
 chemins_info = []
 closest_stop_lines = []
 stop_ids = []
@@ -64,6 +65,7 @@ horaire_info = []
 closest_stop = None
 min_distance = float('inf')
 
+# Traitement des relations entre tronçons et chemins
 for feature in response_relation_troncon_chemin["features"]:
     rs_sv_chem_l = feature['properties']['rs_sv_chem_l']
     rs_sv_tronc_l = feature['properties']['rs_sv_tronc_l']
@@ -71,6 +73,8 @@ for feature in response_relation_troncon_chemin["features"]:
         relation_troncon_chemin_dict[rs_sv_chem_l] = []
     relation_troncon_chemin_dict[rs_sv_chem_l].append(rs_sv_tronc_l)
 
+
+# Identification de l'arrêt de tram le plus proche
 for feature in response_arrets["features"]:
     if feature['properties']['groupe'] == "T_AEROP":
         stop_id = feature['properties']['gid']
@@ -94,6 +98,7 @@ for feature in response_arrets["features"]:
 arrivee_stop_troncons = []
 closest_stop_troncons = []
 
+# Traitement des horaires de tram non réalisés
 for feature in response_horaires["features"]:
     if feature['properties']['source'] == "SAEIV_TRAM" and feature['properties']['etat'] == "NON_REALISE":
         horaire_estime = feature['properties']['hor_estime']
@@ -102,21 +107,27 @@ for feature in response_horaires["features"]:
             horaires_arrets_dict[arret_p] = []
         horaires_arrets_dict[arret_p].append(horaire_estime)
 
+# On trie les horaires pour récupérer uniquement l'horaire la plus proche
 for arret, horaires in horaires_arrets_dict.items():
     horaires.sort()
     horaires_arrets_dict[arret] = horaires[0] if horaires else None
 
+# On récupère les différents tronçons de départ et d'arrivée
 for feature in response_troncon["features"]:
+    # On récupère le tronçon d'arrivée et on l'ajoute à la liste
     if (feature['properties']['vehicule'] == "TRAM"
     and arrets_dict.get(feature['properties']['rg_sv_arret_p_na']) == stop['properties']['libelle']):
         troncon_arrivee_gid = feature['properties']['gid']
         arrivee_stop_troncons.append(troncon_arrivee_gid)
 
+# On récupère le/les tronçons les plus proches de notre position
     if (feature['properties']['vehicule'] == "TRAM"
     and arrets_dict.get(feature['properties']['rg_sv_arret_p_nd']) == closest_stop['properties']['libelle']):
         troncon_gid = feature['properties']['gid']
+        # On récupère le tronçon de départ et on l'ajoute à la liste
         closest_stop_troncons.append(troncon_gid)
 
+# On récupère les données de tous les tronçons
     if feature['properties']['vehicule'] == "TRAM":
         troncon_gid = feature['properties']['gid']
         depart_gid = feature['properties']['rg_sv_arret_p_nd']
@@ -135,6 +146,7 @@ for feature in response_troncon["features"]:
             "geoshape": troncon_geoshape
         }
 
+# On récupère les différentes lignes et on crée un dictionnaire pour attacher chaque identifiant d'une ligne à la ligne de tram correspondante
 for feature in response_lignes["features"]:
     if feature['properties']['vehicule'] == "TRAM":
         ligne_gid = feature['properties']['gid']
@@ -143,9 +155,12 @@ for feature in response_lignes["features"]:
             lignes_dict[ligne_gid] = []
         lignes_dict[ligne_gid].append(ligne_libelle)
 
+
+# On récupère les informations des chemins
 for feature in response_chemins["features"]:
     if (feature['properties']['vehicule'] == "TRAM" 
     and feature['properties']['principal'] == True):
+        # Partie ci-dessous nécessaire pour récupérer la ligne correspondante la plus proche en fonction de l'arrêt
         if closest_stop and feature['properties']['rg_sv_arret_p_nd'] == closest_stop['properties']['gid']:
             closest_stop_lines.append(lignes_dict.get(feature['rs_sv_ligne_a'], "Ligne inconnue"))
         chemin_gid = feature['properties']['gid']
@@ -163,7 +178,11 @@ for feature in response_chemins["features"]:
             "troncon": ligne_troncon
         })
 
+# On trie pour afficher par ID de chemin croissant
 chemins_info_sorted_gid = sorted(chemins_info, key=lambda x: x['chemin_gid'])
+
+
+# Détermination des arrêts de changement et affichage des chemins correspondants
 arret_tram_depart, arret_tram_arrivee = set(), set()
 
 # Collecte des arrêts de départ et d'arrivée pour chaque tronçon pertinent
@@ -171,27 +190,32 @@ for chemin_info in chemins_info_sorted_gid:
     troncon_found = False
     troncon_mene_au_changement = False
     troncon_part_de_changement = False
-    
+
+    # On parcourt tous les tronçons et on vérifie si un des tronçons fait partie de ceux les plus proches    
     for troncon_gid in chemin_info['troncon']:
         if troncon_gid in closest_stop_troncons:
             troncon_found = True
-
+    
+    # Si troncon_found alors on l'ajoute à notre liste d'arrêt de départ
         if troncon_found:
             troncon_info = troncon_dict.get(troncon_gid, {"depart": "Départ inconnu", "arrivee": "Arrivée inconnue"})
             arret_tram_depart.add(troncon_info['depart'])
-            
+    
+    # Si le tronçon est dans la liste du tronçon d'arrivée, on arrête
         if troncon_gid in arrivee_stop_troncons:
             break
     
     # Vérifier si le chemin contient le tronçon d'arrivée avant de commencer à afficher quoi que ce soit
     if any(troncon_gid in arrivee_stop_troncons for troncon_gid in chemin_info['troncon']):
-        #print(f"{chemin_info['chemin_libelle']}: Ligne {chemin_info['ligne_libelle']}")
         for troncon_gid in chemin_info['troncon']:
             troncon_info = troncon_dict.get(troncon_gid, {"depart": "Départ inconnu", "arrivee": "Arrivée inconnue"})
             arret_tram_arrivee.add(troncon_info['depart'])
             
+# On repère les correspondances
 arrets_communs = arret_tram_depart.intersection(arret_tram_arrivee)
 arret_changement = next(iter(arrets_communs), None) 
+
+
 chemins_valides = []
 
 for chemin_info in chemins_info_sorted_gid:
@@ -203,6 +227,7 @@ for chemin_info in chemins_info_sorted_gid:
         if troncon_gid in closest_stop_troncons:
             troncon_found = True
         
+        # On vérifie si le tronçon mène à l'arrêt de changement puis s'il part de l'arrêt de changement
         if troncon_found:
             troncon_info = troncon_dict.get(troncon_gid, {"depart": "Départ inconnu", "arrivee": "Arrivée inconnue"})
             if troncon_info['arrivee'] == arret_changement:
@@ -210,36 +235,39 @@ for chemin_info in chemins_info_sorted_gid:
             if troncon_info['depart'] == arret_changement:
                 troncon_part_de_changement = True
 
+    # Si une des 2 conditions validés, on ajoute ce chemin comme validé à la liste
     if troncon_mene_au_changement or troncon_part_de_changement:
         chemins_valides.append(chemin_info)
 
 
-
+# On parcourt la liste des chemins valides
 for chemin_info in chemins_valides:
     troncon_found = False
-    first_iteration = True
+    first_iteration = True # Nécessaire pour afficher l'horaire de départ d'un seul tram
 
     for troncon_gid in chemin_info['troncon']:
         if troncon_gid in closest_stop_troncons:
             troncon_found = True
-            print(f"{chemin_info['chemin_libelle']}: Ligne {chemin_info['ligne_libelle']}")
+            print(f"{chemin_info['chemin_libelle']}: Ligne {chemin_info['ligne_libelle']}") # Affichage du libellé du chemin et de la ligne correspondante
         if troncon_found:
             troncon_info = troncon_dict.get(troncon_gid, {"depart": "Départ inconnu", "arrivee": "Arrivée inconnue"})
             if first_iteration:
                 current_time = datetime.now()
                 horaire = datetime.fromisoformat(troncon_info['horaire']).replace(tzinfo=None)
-                time_diff = round((horaire - current_time).total_seconds() // 60)
+                time_diff = round((horaire - current_time).total_seconds() // 60) # Permet de récupérer le temps en minutes dans lequel le tram va partir
                 print(f"\033[1m Dans {time_diff} minutes, Départ {troncon_info['depart']} -> Arrivée {troncon_info['arrivee']} \033[0m")
-                first_iteration = False
+                first_iteration = False # On passe à false pour ne plus afficher le temps dans lequel part les autres tram, uniquement celui le plus proche de nous nous intéresse
             else:
+                # Affichage de l'itinéraire jusqu'au changement d'arrêt
                 print(f"Départ {troncon_info['depart']} -> Arrivée {troncon_info['arrivee']}")
+
 
             if arret_changement == troncon_info['arrivee']:
                 for chemin_info in [chemin_info for chemin_info in chemins_info_sorted_gid if any(troncon_gid in arrivee_stop_troncons for troncon_gid in chemin_info['troncon'])][:1]:
-                    start_printing = False 
+                    start_printing = False # Flag pour savoir si on doit afficher ou non le trajet
                     for troncon_gid in chemin_info['troncon']:
                         troncon_info = troncon_dict.get(troncon_gid, {"depart": "Départ inconnu", "arrivee": "Arrivée inconnue"})
-                        # Commencer à imprimer à partir de l'arrêt de changement
+                        # Commencer à afficher à partir de l'arrêt de changement
                         if troncon_info['depart'] == arret_changement or start_printing:
                             if not start_printing:
                                 print(f"\033[1m Changement d'arrêt : {arret_changement}, Ligne {chemin_info['ligne_libelle']} \033[0m")
--- README.md
+++ README.md
@@ -1,9 +1,32 @@
-# Traitement des horaires de tramway de Bordeaux
+# Traitement des horaires de tramway de Bordeaux via API
 
-## Description
-Ce script est conçu pour interagir avec l'API GeoJSON de l'atelier OPENDATA de Bordeaux Métropole afin de récupérer et traiter les données des différentes lignes de tramway, incluant les arrêts, les itinéraires, les tronçons et les horaires. Il identifie les arrêts de tram les plus proches d'une position GPS donnée, recueille les premiers horaires de passage estimés pour chaque arrêt, et traite les informations des itinéraires et tronçons de tramway pour une analyse ou une visualisation ultérieure.
+## Description du projet
+Ce projet est conçu pour interagir avec l'API GeoJSON de l'atelier OPENDATA de Bordeaux Métropole afin de récupérer et traiter les données des différentes lignes de tramway, incluant les arrêts, les itinéraires, les tronçons et les horaires. Il identifie les arrêts de tram les plus proches d'une position GPS donnée, recueille les premiers horaires de passage estimés pour chaque arrêt, et traite les informations des itinéraires et tronçons de tramway pour une analyse ou une visualisation ultérieure.
 
-## Fonctionnalités
+## Définition de l'Open data et Histoire
+
+Tout d'abord, nous devons définir ce qu'est l'open data.L'open data se réfère à des données accessibles en libre accès, pouvant être utilisées et partagées sans contrainte. Pour être considérée comme ouverte, une donnée doit être accessible, réutilisable et redistribuable par tout utilisateur, sans restriction. Ces critères garantissent son interopérabilité avec des données provenant de différentes sources.
+
+Ce concept englobe une multitude de secteurs, allant de la géolocalisation à la finance, en passant par les sciences, le transport, la culture, le sport, la santé, l'environnement, et bien d'autres encore.
+
+### Histoire en France de l'Open data
+
+L'accès aux données constitue un enjeu crucial pour le fonctionnement démocratique d'une société. En France, depuis 1978, la loi CADA autorise le public à accéder aux informations détenues par l'administration.
+
+Le concept de données ouvertes a émergé à la fin des années 90, avec l'expression "open data" apparaissant pour la première fois en 1995 dans une publication scientifique du National Research Council américain. Cette notion s'inscrit dans une réflexion sur la nature des données en tant que bien commun et sur les risques de privatisation des connaissances.
+
+Il faudra attendre 2005 pour qu'un cadre juridique du soit établi avec l'adoption de l'Open Definition, étendant les principes de l'Open Source aux connaissances. Ces principes reposent sur l'accessibilité, la redistribution et la réutilisation des données par tous, sans discrimination.
+
+La formalisation des principes de l'Open Data intervient deux ans plus tard lors de la rencontre de Sebastopol, définissant huit critères pour qualifier une donnée comme ouverte. Aux États-Unis, ces principes sont adoptés par le gouvernement en 2009 dans le cadre de l'Open Government.
+
+En 2010, la France lance le portail national d'accès aux informations, data.gouv.fr, permettant aux citoyens d'accéder aux ressources de l'administration. La mission Etalab en assure la maintenance depuis sa création en décembre 2011.
+
+En 2013, la charte internationale sur l'Open Data, issue du G8, confère une dimension politique à l'ouverture des données, s'inscrivant dans la continuité des initiatives précédentes.
+
+Aujourd'hui, de nombreux gouvernements à travers le monde reprennent les principes des données ouvertes, la France étant reconnue comme l'un des leaders mondiaux en matière d'ouverture des données publiques.
+
+
+## Fonctionnalités du projet
 - **Identification de l'arrêt le plus proche** : Détermine l'arrêt de tram le plus proche d'une position GPS donnée.
 - **Traitement des horaires** : Récupère le premier horaire estimé pour les arrêts de tram.
 - **Analyse des itinéraires et tronçons** : Collecte et traite les données des itinéraires et tronçons de tramway pour identifier les connexions et parcours pertinents.
@@ -39,7 +62,37 @@ pip install -r requirements.txt
 
 ## Fonctions principales
 - `get_geojson(url)`: Récupère les données depuis l'endpoint API GeoJSON spécifié.
+
+```
+def get_geojson(url):
+    response = requests.get(url)
+    if response.status_code == 200: #On récupère la réponse get
+        return response.json() #On retourne un json
+    else:
+        print(f"Erreur pour charger les données de {url}") #Si on arrive pas à récupérer alors msg d'erreur
+        return None
+```
+
 - `haversine_distance(lat1, lon1, lat2, lon2)`: Calcule la distance entre deux coordonnées GPS.
+
+```
+def haversine_distance(lat1, lon1, lat2, lon2):
+    # Convertir les degrés en radians
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+    
+    # Différence des coordonnées
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    
+    # Formule de Haversine
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.asin(math.sqrt(a))
+    
+    # Rayon de la Terre en kilomètres
+    r = 6371
+    
+    return c * r
+```
 
 ## Exemples de sorties
 - Liste des arrêts de tram les plus proches avec l'horaire de passage en minutes "Dans X minutes" de l'arrêt de tram le plus proche.
@@ -58,4 +111,4 @@ Ce script est à des fins éducatives uniquement. Les applications en temps rée
 Les données utilisées par ce script sont fournies par Bordeaux Métropole via son API GeoJSON. L'utilisation de ces données est soumise aux termes et conditions de Bordeaux Métropole. Les utilisateurs du script doivent également se conformer à ces termes et vérifier les conditions d'utilisation sur le [site officiel](https://opendata.bordeaux-metropole.fr/pages/accueil/) avant d'utiliser les données.
 
 ## Contributions
-Les contributions au projet sont les bienvenues, mais les contributeurs doivent également s'assurer de respecter les droits d'auteur et les licences des bibliothèques ou des ressources tierces utilisées dans ce projet.
+Les contributions au projet sont les bienvenues, mais les contributeurs doivent également s'assurer de respecter les licences des bibliothèques ou des ressources tierces utilisées dans ce projet.
